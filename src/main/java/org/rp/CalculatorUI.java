bd79379bbc3f37780c6eb62482ec3beb7827af2b
Reetta Puska <reetta.puska@helsinki.fi>
2024-09-03 18:24:43 +0300

    init project

---
+++ src/main/java/org/rp/CalculatorUI.java
@@ -0,0 +1,34 @@
+package org.rp;
+
+import java.util.Scanner;
+
+public class CalculatorUI {
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the calculator!");
+        takeInput(scanner);
+    }
+
+    private void takeInput(Scanner scanner) {
+        while (true) {
+            System.out.println("write an expression or ? for help\n> ");
+            String input = scanner.nextLine();
+            boolean quit = handleInput(input);
+            if (quit) break;
+        }
+
+    }
+
+    private boolean handleInput(String input) {
+        if (input.equals("?")){
+            System.out.println("good luck!");
+        } else if (input.equals("q")) {
+            System.out.println("bye!");
+            return true;
+        } else {
+            System.out.println("Can't handle that yet!");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/rp/Main.java
@@ -0,0 +1,7 @@
+package org.rp;
+
+public class Main {
+    public static void main(String[] args) {
+        new CalculatorUI().run();
+    }
+}
